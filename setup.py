dc1b1e068eafb8f5241f66666f636b2843adaba4
Mark Unsworth <mark@knows.is>
2015-03-25 15:31:13 +0000

    corrected package name

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author='Knowsis Ltd',
     author_email='mark@knowsis.com',
     dependency_links=[],
-    install_requires=['requests', 'simplejson', 'oauth2', 'dateutil'],
+    install_requires=['requests', 'simplejson', 'oauth2', 'python-dateutil'],
     tests_require=[],
     url="https://github.com/knowsis/pyknowsis"
 )
